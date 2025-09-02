2e40b341c044d8b86f2d928e5e96bd52d5647323
tomas nieto <tomasnieto249@gmail.com>
2025-09-02 20:02:08 -0300

    estoy hacienso las screens

---
+++ abuelaconecta/app/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) return null;
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="HomeScreen" />
+        <Stack.Screen name="ProfileScreen" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
